e45f21bdc7925948d82f97f4d1bab0feb163c629
Caleb Provost <Caleb.Provost@Colorado.edu>
2022-10-06 23:21:43 -0400

    Add I2C drivers testing baseboard's Si7021 sensor
    Signed-off-by: Caleb Provost <Caleb.Provost@Colorado.edu>

--- autogen/sl_event_handler.c
+++ autogen/sl_event_handler.c
@@ -13,10 +13,19 @@
 #include "btl_interface.h"
 #include "sl_board_control.h"
 #include "sl_sleeptimer.h"
+#include "app_log.h"
 #include "sl_bluetooth.h"
 #include "sl_debug_swo.h"
+#include "gpiointerrupt.h"
+#include "sl_i2cspm_instances.h"
+#include "sl_iostream_stdlib_config.h"
+#include "sl_iostream_init_usart_instances.h"
 #include "sl_mbedtls.h"
 #include "sl_mpu.h"
+#include "sl_simple_button_instances.h"
+#include "sl_simple_timer.h"
+#include "sl_cli_instances.h"
+#include "sl_iostream_init_instances.h"
 #include "sl_power_manager.h"
 #include "sl_cos.h"
 
@@ -38,6 +47,9 @@ void sl_platform_init(void)
 void sl_driver_init(void)
 {
   sl_debug_swo_init();
+  GPIOINT_Init();
+  sl_i2cspm_init_instances();
+  sl_simple_button_init_instances();
   sl_cos_send_config();
 }
 
@@ -45,8 +57,11 @@ void sl_service_init(void)
 {
   sl_board_configure_vcom();
   sl_sleeptimer_init();
+  sl_iostream_stdlib_disable_buffering();
   sl_mbedtls_init();
   sl_mpu_disable_execute_from_ram();
+  sl_iostream_init_instances();
+  sl_cli_instances_init();
 }
 
 void sl_stack_init(void)
@@ -58,6 +73,7 @@ void sl_stack_init(void)
 
 void sl_internal_app_init(void)
 {
+  app_log_init();
 }
 
 void sl_platform_process_action(void)
@@ -66,6 +82,8 @@ void sl_platform_process_action(void)
 
 void sl_service_process_action(void)
 {
+  sli_simple_timer_step();
+  sl_cli_instances_tick();
 }
 
 void sl_stack_process_action(void)
@@ -77,3 +95,7 @@ void sl_internal_app_process_action(void)
 {
 }
 
+void sl_iostream_init_instances(void)
+{
+  sl_iostream_usart_init_instances();
+}
